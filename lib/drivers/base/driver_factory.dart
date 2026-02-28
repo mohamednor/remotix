3ec0496e3c98b6a28f2e9ddb3012da377f686eea
mohamednor <mohamedelshref55@gmail.com>
2026-03-01 01:36:17 +0200

    Update driver_factory.dart

--- lib/drivers/base/driver_factory.dart
+++ lib/drivers/base/driver_factory.dart
@@ -1,27 +1,21 @@
 import '../../domain/entities/device.dart';
 import 'tv_driver.dart';
-
 import '../lg/lg_webos_driver.dart';
 import '../samsung/samsung_tizen_driver.dart';
 import '../androidtv/android_tv_driver.dart';
 
 class DriverFactory {
   static TvDriver create(Device device) {
-    final label = (device.typeLabel).toLowerCase();
-
-    if (label.contains('webos') || label.contains('lg')) {
-      return LgWebOsDriver(device.ipAddress);
+    switch (device.type) {
+      case DeviceType.lgWebOs:
+        return LgWebOsDriver(device.ipAddress);
+      case DeviceType.samsungTizen:
+        return SamsungTizenDriver(device.ipAddress);
+      case DeviceType.androidTv:
+        return AndroidTvDriver(device.ipAddress);
+      case DeviceType.unknown:
+        // خلي unknown يحاول LG أو Samsung؟ الأفضل: خليه Samsung مؤقتًا أو اعمل UI يطلب اختيار النوع
+        return SamsungTizenDriver(device.ipAddress);
     }
-
-    if (label.contains('tizen') || label.contains('samsung')) {
-      return SamsungTizenDriver(device.ipAddress);
-    }
-
-    if (label.contains('android') || label.contains('google')) {
-      return AndroidTvDriver(device.ipAddress);
-    }
-
-    // Default:
-    return SamsungTizenDriver(device.ipAddress);
   }
 }
