9be120addeb8ab0edbc93f79ba7b1d969ab43b28
mohamednor <mohamedelshref55@gmail.com>
2026-03-02 01:04:46 +0200

    Update driver_factory.dart

--- lib/drivers/base/driver_factory.dart
+++ lib/drivers/base/driver_factory.dart
@@ -1,3 +1,5 @@
+// lib/drivers/base/driver_factory.dart
+
 import '../../domain/entities/device.dart';
 import 'tv_driver.dart';
 import '../androidtv/android_tv_driver.dart';
@@ -9,28 +11,36 @@ class DriverFactory {
     switch (device.type) {
       case DeviceType.lgWebOs:
         return LgWebOsDriver(device.ipAddress);
+
       case DeviceType.samsungTizen:
         return SamsungTizenDriver(device.ipAddress);
+
       case DeviceType.androidTv:
         return AndroidTvDriver(device.ipAddress);
+
       case DeviceType.unknown:
       default:
-        // fallback عشان التطبيق ما يقعش لو الاكتشاف رجّع Unknown
-        final s =
-            '${device.manufacturer} ${device.model} ${device.name}'.toLowerCase();
+        // Fallback ذكي لو discovery رجّع Unknown
+        final manufacturer = device.manufacturer ?? '';
+        final model = device.model ?? '';
+        final name = device.name ?? '';
+
+        final s = '$manufacturer $model $name'.toLowerCase();
 
         if (s.contains('lg') || s.contains('webos')) {
           return LgWebOsDriver(device.ipAddress);
         }
+
         if (s.contains('samsung') || s.contains('tizen')) {
           return SamsungTizenDriver(device.ipAddress);
         }
+
         if (s.contains('android')) {
           return AndroidTvDriver(device.ipAddress);
         }
 
         throw Exception(
-          'Unsupported TV type: ${device.typeLabel} (${device.ipAddress})',
+          'Unsupported TV type: ${device.type} (${device.ipAddress})',
         );
     }
   }
