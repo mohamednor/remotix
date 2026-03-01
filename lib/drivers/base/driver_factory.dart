c0324841c0a55a48a1fe1e23fe8483cd85c324d1
mohamednor <mohamedelshref55@gmail.com>
2026-03-01 18:22:41 +0200

    Update driver_factory.dart

--- lib/drivers/base/driver_factory.dart
+++ lib/drivers/base/driver_factory.dart
@@ -1,12 +1,11 @@
 // lib/drivers/base/driver_factory.dart
 
 import '../../domain/entities/device.dart';
-import 'tv_driver.dart';
-
 import '../lg/lg_webos_driver.dart';
 import '../samsung/samsung_tizen_driver.dart';
 import '../androidtv/android_tv_driver.dart';
 import '../../core/utils/app_logger.dart';
+import 'tv_driver.dart';
 
 class DriverFactory {
   static TvDriver create(Device device) {
